5dc877f2392ddbfa6e23a6d750432eb28c22689e
Jason Xue <jasonxue1228@gmail.com>
2020-06-06 20:42:20 -0600

    Added STOPPED count.

--- frontend/gui/src/components/FlightsInfo.js
+++ frontend/gui/src/components/FlightsInfo.js
@@ -4,11 +4,12 @@ import Typography from "@material-ui/core/Typography";
 import selectFlights from "../selector/selectFlights";
 import {CircleNumber} from "./InvertedText";
 
-export const FlightsInfo = ({flightsCount, runningFlightsCount, failedFlightsCount}) => {
+export const FlightsInfo = ({flightsCount, runningFlightsCount, failedFlightsCount, stoppedFlightsCount}) => {
     return (
         <Typography variant="h3" style={{fontWeight: "bolder", color: "darkblue", margin: "0.2em"}}>
             {"TOTALLY"} <CircleNumber style={{backgroundColor: "grey"}}>{flightsCount}</CircleNumber>{" flights found ! "}
                  <CircleNumber style={{backgroundColor: "blue"}}>{runningFlightsCount}</CircleNumber>{" RUNNING ! "}
+                 <CircleNumber style={{backgroundColor: "brown"}}>{stoppedFlightsCount}</CircleNumber>{" STOPPED ! "}
                   <CircleNumber style={{backgroundColor: "red"}}>{failedFlightsCount}</CircleNumber>{" FAILED !"}
         </Typography>
     )
@@ -18,11 +19,13 @@ const mapStateToProps = (state) => {
     const visibleFlights = selectFlights(state.flights, state.filters);
     const runningFlights = visibleFlights.filter((item) => (item.current_start));
     const failedFlights = visibleFlights.filter((item) => (item.failure_steps_count));
+    const stoppedFlights = visibleFlights.filter((item) => (item.current_step_name.includes('Stopped')));
 
     return {
         flightsCount: visibleFlights.length,
         runningFlightsCount: runningFlights.length,
         failedFlightsCount: failedFlights.length,
+        stoppedFlightsCount: stoppedFlights.length,
     }
 };
 
