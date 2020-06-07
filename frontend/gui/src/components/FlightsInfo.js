4866cc3e0b7c7737a3f246fedc54499a6ab6a739
Jason Xue <jasonxue1228@gmail.com>
2020-06-06 20:53:43 -0600

    Added SUCCESS count.

--- frontend/gui/src/components/FlightsInfo.js
+++ frontend/gui/src/components/FlightsInfo.js
@@ -4,13 +4,17 @@ import Typography from "@material-ui/core/Typography";
 import selectFlights from "../selector/selectFlights";
 import {CircleNumber} from "./InvertedText";
 
-export const FlightsInfo = ({flightsCount, runningFlightsCount, failedFlightsCount, stoppedFlightsCount}) => {
+export const FlightsInfo = ({
+                                flightsCount, runningFlightsCount, failedFlightsCount, stoppedFlightsCount,
+                                successFlightsCount
+                            }) => {
     return (
         <Typography variant="h3" style={{fontWeight: "bolder", color: "darkblue", margin: "0.2em"}}>
-            {"TOTALLY"} <CircleNumber style={{backgroundColor: "grey"}}>{flightsCount}</CircleNumber>{" flights found ! "}
-                 <CircleNumber style={{backgroundColor: "blue"}}>{runningFlightsCount}</CircleNumber>{" RUNNING ! "}
-                 <CircleNumber style={{backgroundColor: "brown"}}>{stoppedFlightsCount}</CircleNumber>{" STOPPED ! "}
-                  <CircleNumber style={{backgroundColor: "red"}}>{failedFlightsCount}</CircleNumber>{" FAILED !"}
+            <CircleNumber style={{backgroundColor: "grey"}}>{flightsCount}</CircleNumber>{" TOTAL ! "}
+            <CircleNumber style={{backgroundColor: "green"}}>{successFlightsCount}</CircleNumber>{" SUCCESS ! "}
+            <CircleNumber style={{backgroundColor: "blue"}}>{runningFlightsCount}</CircleNumber>{" RUNNING ! "}
+            <CircleNumber style={{backgroundColor: "brown"}}>{stoppedFlightsCount}</CircleNumber>{" STOPPED ! "}
+            <CircleNumber style={{backgroundColor: "red"}}>{failedFlightsCount}</CircleNumber>{" FAILED !"}
         </Typography>
     )
 };
@@ -20,12 +24,14 @@ const mapStateToProps = (state) => {
     const runningFlights = visibleFlights.filter((item) => (item.current_start));
     const failedFlights = visibleFlights.filter((item) => (item.failure_steps_count));
     const stoppedFlights = visibleFlights.filter((item) => (item.current_step_name.includes('Stopped')));
+    const successFlights = visibleFlights.filter((item) => (item.current_step_name.includes('All Done')));
 
     return {
         flightsCount: visibleFlights.length,
         runningFlightsCount: runningFlights.length,
         failedFlightsCount: failedFlights.length,
         stoppedFlightsCount: stoppedFlights.length,
+        successFlightsCount: successFlights.length,
     }
 };
 
