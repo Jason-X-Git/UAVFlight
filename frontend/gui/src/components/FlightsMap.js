7e3a4fbf5c6a72b8c278708c7f9e24178de8460f
Jason Xue <jasonxue1228@gmail.com>
2020-06-21 16:27:06 -0600

    Replace loadData promise with async/await, try/catch block.

--- frontend/gui/src/components/FlightsMap.js
+++ frontend/gui/src/components/FlightsMap.js
@@ -39,126 +39,117 @@ const MapView = (props) => {
 
     const classes = useStyles();
 
-    const loadData = (flights) => {
-        const options = {
-            url: "https://js.arcgis.com/4.15/init.js",
-            css: "https://js.arcgis.com/4.15/esri/themes/light/main.css",
-        };
+    const loadData = async (flights) => {
+        try {
+            const options = {
+                url: "https://js.arcgis.com/4.15/init.js",
+                css: "https://js.arcgis.com/4.15/esri/themes/light/main.css",
+            };
+            const [Map,
+                MapView,
+                CoordinateConversion,
+                Search,
+                Graphic,
+                GraphicsLayer,
+                BasemapGallery] = await esriLoader
+                .loadModules(
+                    [
+                        "esri/Map",
+                        "esri/views/MapView",
+                        "esri/widgets/CoordinateConversion",
+                        "esri/widgets/Search",
+                        "esri/Graphic",
+                        "esri/layers/GraphicsLayer",
+                        "esri/widgets/BasemapGallery"
+                    ],
+                    options
+                );
 
-        esriLoader
-            .loadModules(
-                [
-                    "esri/Map",
-                    "esri/views/MapView",
-                    "esri/layers/Layer",
-                    "esri/widgets/CoordinateConversion",
-                    "esri/widgets/Search",
-                    "esri/Graphic",
-                    "esri/layers/GraphicsLayer",
-                    "esri/widgets/BasemapToggle",
-                    "esri/widgets/BasemapGallery"
-                ],
-                options
-            )
-            .then(
-                ([
-                     Map,
-                     MapView,
-                     Layer,
-                     CoordinateConversion,
-                     Search,
-                     Graphic,
-                     GraphicsLayer,
-                     BasemapToggle,
-                     BasemapGallery,
-                 ]) => {
-                    // doSomeThing
-
-                    const map = new Map({
-                        basemap: "streets",
-                    });
-
-                    const view = new MapView({
-                        container: "mapDiv",
-                        map: map,
-                        center: [-115, 55],
-                        zoom: 5,
-                    });
-
-                    // const basemapToggle = new BasemapToggle({
-                    //     view: view,
-                    //     nextBasemap: "satellite",
-                    // });
-                    //
-                    // view.ui.add(basemapToggle, "bottom-right");
-
-                    const basemapGallery = new BasemapGallery({
-                        view: view,
-                        // container: "baseGalleryDiv",
-                        activeBasemap: "satellite",
-                    });
-                    view.ui.add(basemapGallery, "bottom-right");
-
-                    const coordinateConversionWidget = new CoordinateConversion({
-                        view: view,
-                    });
-                    const searchWidget = new Search({
-                        view: view,
-                    });
-
-                    view.ui.components = ["attribution", "compass", "zoom"];
-
-                    view.ui.add(coordinateConversionWidget, {
-                        position: "bottom-left",
-                        index: 0,
-                    });
-                    view.ui.add(searchWidget, {position: "top-right", index: 1});
-
-                    const graphicsLayer = new GraphicsLayer();
-                    map.add(graphicsLayer);
-
-                    const addPoint = (point) => {
-                        console.log(`Adding point ${point.uav_no} ${point.latitude} ${point.longitude}`);
-                        var simpleMarkerSymbol = {
-                            type: "simple-marker",
-                            color: point.latest_status ? colorTypes[point.latest_status] : 'black', // orange
-                            outline: {
-                                color: [255, 255, 255], // white
-                                width: 1,
-                            },
-                        };
-                        const pointGraphic = new Graphic({
-                            geometry: point,
-                            symbol: simpleMarkerSymbol,
-                        });
-
-                        const textGraphic = new Graphic({
-                            geometry: point,
-                            symbol: {
-                                type: "text",
-                                color: [25, 25, 25],
-                                haloColor: [255, 255, 255],
-                                haloSize: "2px",
-                                text: point.uav_no,
-                                xoffset: 0,
-                                yoffset: -25,
-                                font: {
-                                    size: 12,
-                                    weight: "bolder",
-                                },
-                            },
-                        });
-
-                        graphicsLayer.add(pointGraphic);
-                        graphicsLayer.add(textGraphic);
-                    };
-
-                    flights.map((point) => addPoint(point));
-                }
-            )
-            .catch((err) => {
-                console.error("Failed to initialize !!!", err);
+            const map = new Map({
+                basemap: "streets",
             });
+
+            const view = new MapView({
+                container: "mapDiv",
+                map: map,
+                center: [-115, 55],
+                zoom: 5,
+            });
+
+            // const basemapToggle = new BasemapToggle({
+            //     view: view,
+            //     nextBasemap: "satellite",
+            // });
+            //
+            // view.ui.add(basemapToggle, "bottom-right");
+
+            const basemapGallery = new BasemapGallery({
+                view: view,
+                // container: "baseGalleryDiv",
+                activeBasemap: "satellite",
+            });
+            view.ui.add(basemapGallery, "bottom-right");
+
+            const coordinateConversionWidget = new CoordinateConversion({
+                view: view,
+            });
+            const searchWidget = new Search({
+                view: view,
+            });
+
+            view.ui.components = ["attribution", "compass", "zoom"];
+
+            view.ui.add(coordinateConversionWidget, {
+                position: "bottom-left",
+                index: 0,
+            });
+            view.ui.add(searchWidget, {position: "top-right", index: 1});
+
+            const graphicsLayer = new GraphicsLayer();
+            map.add(graphicsLayer);
+
+            const addPoint = (point) => {
+                console.log(`Adding point ${point.uav_no} ${point.latitude} ${point.longitude}`);
+                const simpleMarkerSymbol = {
+                    type: "simple-marker",
+                    color: point.latest_status ? colorTypes[point.latest_status] : 'black', // orange
+                    outline: {
+                        color: [255, 255, 255], // white
+                        width: 1,
+                    },
+                };
+                const pointGraphic = new Graphic({
+                    geometry: point,
+                    symbol: simpleMarkerSymbol,
+                });
+
+                const textGraphic = new Graphic({
+                    geometry: point,
+                    symbol: {
+                        type: "text",
+                        color: [25, 25, 25],
+                        haloColor: [255, 255, 255],
+                        haloSize: "2px",
+                        text: point.uav_no,
+                        xoffset: 0,
+                        yoffset: -25,
+                        font: {
+                            size: 12,
+                            weight: "bolder",
+                        },
+                    },
+                });
+
+                graphicsLayer.add(pointGraphic);
+                graphicsLayer.add(textGraphic);
+            };
+
+            flights.map((point) => addPoint(point));
+        } catch (e) {
+            console.error(e)
+        } finally {
+            console.log('Cannot load ArcGIS Javascript API stuff !')
+        }
     };
 
     useEffect(() => {
